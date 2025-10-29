ab45fe72926b6d0ac5317793f319a818e9aa978e
Aadityyaaaa-0410 <aadityasingh2108@gmail.com>
2025-10-29 18:42:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import TodoList from "./components/TodoList";
 function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
-  const API_BASE = "https://app-alb-270296790.us-east-1.elb.amazonaws.com";
+  const API_BASE = "http://app-alb-270296790.us-east-1.elb.amazonaws.com";
  // const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
   // ✅ Fetch all todos
