b7acccd2e7d6a0117f6be880e6a74834206231d5
Aadityyaaaa-0410 <aadityasingh2108@gmail.com>
2025-10-30 15:34:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./db');
-//synced12
+//synced12 3
 const app = express();
 app.use(bodyParser.json());
 app.use(cors()); // Enable CORS for frontend calls
